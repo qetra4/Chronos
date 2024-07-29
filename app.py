9feed2e7ef23f95dd420391b554972a2337225ed
ioanna <andofilopino@inbox.ru>
2024-07-29 15:47:27 +0300

    Настроена платежная система с использованием тестового токена. Нужен рефакторинг

--- app.py
+++ app.py
@@ -2,20 +2,21 @@ import asyncio
 import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
-from handlers.user_private import user_private_router
+from handlers.common_handlers import user_private_router
 import logging
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 
-ALLOWED_UPDATES = ['message, edited_message']
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
 dp.include_router(user_private_router)
 logging.basicConfig(level=logging.INFO)
 
+
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
 
 asyncio.run(main())
---
+++ messages.py
@@ -0,0 +1,20 @@
+item_title = 'Пожертвование во процветание религии'
+
+item_description = '''\
+Очень благой поступок
+'''
+
+start = '''
+Доброго времени суток! Я - бот, призванный собирать пожертвования во славу Летающего Макаронного Монстра. Хотите сделать пожертвование?
+'''
+
+successful_payment = '''
+Пожертвование на сумму `{total_amount} {currency}` совершено успешно!
+'''
+
+MESSAGES = {
+    'start': start,
+    'item_title': item_title,
+    'item_description': item_description,
+    'successful_payment': successful_payment,
+}
