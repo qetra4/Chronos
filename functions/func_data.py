51432af00f5330377a6f4f0ff94e3f944126fde0
qetra4 <andofilopino@inbox.ru>
2025-02-12 15:26:10 +0300

    Graphics design was updated

--- functions/func_data.py
+++ functions/func_data.py
@@ -6,23 +6,25 @@ async def pie_hours_extra(vals, labels):
     for i in range(len(vals)):
         if labels[i] != 'Да' and labels[i] != "Нет":
             indexes_to_remove.append(i)
-    print(indexes_to_remove)
     final_vals = [item for idx, item in enumerate(vals) if idx not in indexes_to_remove]
     final_labels = [item for idx, item in enumerate(labels) if idx not in indexes_to_remove]    
     fig, ax = plt.subplots()
-    total = sum(final_vals)
-    percentages = [(val / total) * 100 for val in final_vals]
     def func(pct, s, val):
         return f"{val} {s} ({pct:.1f}%)"
+    colors = ['skyblue', 'cornflowerblue']
     wedges, texts, autotexts = ax.pie(
         final_vals, 
         labels=final_labels, 
         autopct=lambda pct: func(pct, "часов\n", int(final_vals[int(pct / 100 * len(final_vals))])),
-        startangle=90
+        startangle=90,
+        colors=colors,
         )
-    for text in autotexts:
-        text.set_color('white')
     ax.axis('equal')
+    plt.title(
+    label="Соотношение часов дополнительных работ и основных", 
+    fontdict={"fontsize":14},
+    pad=20)
+    plt.tight_layout()
     plt.show()
 
 
@@ -34,7 +36,8 @@ async def hist_hours_by_objects(vals, labels):
     final_vals = [item for idx, item in enumerate(vals) if idx not in indexes_to_remove]
     final_labels = [item for idx, item in enumerate(labels) if idx not in indexes_to_remove]    
     fig, ax = plt.subplots()
-    bar_container = ax.bar(final_labels, final_vals)
+    colors = ['skyblue', 'steelblue', 'deepskyblue', 'powderblue',  'cornflowerblue', 'lightskyblue', 'dodgerblue', 'lightcyan']
+    bar_container = ax.bar(final_labels, final_vals, color=colors)
     ax.set(ylabel='Часы', title='Распределение человекочасов по объектам')
     ax.bar_label(bar_container, fmt='{:,.0f}')
     plt.tight_layout()
